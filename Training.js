204a00ff5494dba138266f24089d5c6ac46fe99d
Stanlee Okwii <stanleeparker12@gmail.com>
2017-07-06 13:14:44 +0300

    Initial commit

---
+++ Training.js
@@ -0,0 +1,14 @@
+var ar1 = [1,2,3,4,5];
+
+ var functionArray = ar1.map(function (arrayItem,index,ar1){
+     const previousValue = index === 0 ? 0:ar1[index - 1];
+     const currentVlaue = 2;
+ });
+
+function addd(z,y){
+sum = z+y;
+console.log(sum)
+return sum;
+}
+addd(2,3);
+ 
